f4a50dd92646fef4d127654e2c9a65f5086adaf9
Hamberfim <adhamlingithub@gmail.com>
2024-01-02 16:40:29 -0600

    branching

--- flow/src/main.rs
+++ flow/src/main.rs
@@ -39,4 +39,25 @@ fn main() {
     };
 
     println!("The result is {result}");
+
+    // nested loop
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
 }
