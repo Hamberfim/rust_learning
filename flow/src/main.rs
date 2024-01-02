b393b78789018ed42ad451f1ef686bce0ddc54dd
Hamberfim <adhamlingithub@gmail.com>
2024-01-02 16:27:10 -0600

    branching

--- flow/src/main.rs
+++ flow/src/main.rs
@@ -11,4 +11,19 @@ fn main() {
     if number != 0 {
         println!("Number is something other than zero.");
     }
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    // if in a let statement
+    let condition: bool = false;
+    let new_number: i32 = if condition { 4 } else { number };
+    println!("The value of the number is {new_number}");
 }
