c6133986fa151b86f84e9bc7ae92c1024be75f76
Hamberfim <adhamlingithub@gmail.com>
2024-01-02 14:36:54 -0600

    functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    // https://rust-book.cs.brown.edu/ch03-03-how-functions-work.html
+    println!("Hello, from main!");
+    // another_func(5);
+
+    print_labeled_measurements(3, 'h');
+}
+
+// fn another_func(x: i32) {
+//     println!("Hello from 'Another function' who param is {x}.");
+// }
+
+fn print_labeled_measurements(num_value: i32, unit_label: char) {
+    println!("The measurement is: {num_value}{unit_label}");
 }
