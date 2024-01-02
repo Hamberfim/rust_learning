8bb7d60da785ada0c3d4aa6422586c932bb4fcc8
Hamberfim <adhamlingithub@gmail.com>
2024-01-02 15:23:37 -0600

    functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -3,6 +3,10 @@ fn main() {
     // another_func(5);
 
     print_labeled_measurements(3, 'h');
+
+    let x = plus_one(5);
+
+    println!("The value of x is: {x}");
 }
 
 // fn another_func(x: i32) {
@@ -11,4 +15,9 @@ fn main() {
 
 fn print_labeled_measurements(num_value: i32, unit_label: char) {
     println!("The measurement is: {num_value}{unit_label}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    // an expression, no semicolon!
+    x + 1
 }
