98d83a56676ee1ca050d16ccce436d436aa229c7
Hamberfim <adhamlingithub@gmail.com>
2024-01-02 16:21:02 -0600

    branching

---
+++ flow/src/main.rs
@@ -0,0 +1,14 @@
+// control flow
+
+fn main() {
+    let number: i32 = 7;
+    if number < 5 {
+        println!("Condition was true.");
+    } else {
+        println!("Condition was false.");
+    }
+
+    if number != 0 {
+        println!("Number is something other than zero.");
+    }
+}
